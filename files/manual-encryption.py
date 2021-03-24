81ded610f686595f045d460a2b97ae35ccbdb065
kurisukun <barroshenriques.chris@gmail.com>
2021-03-24 15:23:36 +0100

    final report

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -8,7 +8,7 @@ from rc4 import RC4
 #Cle wep AA:AA:AA:AA:AA
 key= b'\xaa\xaa\xaa\xaa\xaa'
 
-m = binascii.hexlify(b'Message forged')
+m = b"\xaa\xaa\x03\x00\x00\x00\x08\x06\x00\x01\x08\x00\x06\x04\x00\x01\x90'\xe4\xeaa\xf2\xc0\xa8\x01d\x00\x00\x00\x00\x00\x10\xc0\xa8\x01\xc8"
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]
